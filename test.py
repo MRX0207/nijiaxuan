d443693a81fc24eb18d8b6bf797d44e9b9b4e692
MRX0207 <335034879@qq.com>
2020-03-29 17:47:57 +0800

    s

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'nijiaxuan'
+
+
+class TestClass(object):
+
+    def __init__(self,val=1):
+        self.value=val
+
+    def __str__(self):
+        return str(self.value)
+
+    __repr__ = __str__
+
+
